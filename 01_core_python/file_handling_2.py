6d1b7ff54927ec7feac8e0fff2028b59716f7227
T BHUPAL REDDY <thandlambhupal8@gmail.com>
2026-07-27 21:04:31 +0530

    file_io_exercises

--- 01_core_python/file_handling_2.py
+++ 01_core_python/file_handling_2.py
@@ -8,9 +8,27 @@ with open(path,"r") as students:
 
 
 
-with open(path,"a") as students:
-    students.write("\nZane,Computer Science,A") # adds the student at very last line
+# with open(path,"a") as students:
+#     students.write("\nZane,Computer Science,A") 
+# adds the student at very last line
+# if run every time every time the student will be added thats why commented!
 
 with open(path,"r") as student_101:
     student_data=student_101.read()
     print(student_data)
+
+print()
+
+student_record=[]
+with open(path,"r") as students:
+    for student in students:
+       student_record.append(student.strip().split(",")) # this splits the every row in to 2d list and stores in student_record
+
+print()
+
+print(student_record[0]) # takes the first record in list
+
+print()
+for student in student_record:
+     print(student[0]) # prints the every student names in the list;
+
